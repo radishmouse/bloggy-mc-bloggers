de48fdd3abc0a700e1852ca407551ab58b7fe0e2
Chris Aquino <chris.aquino@gmail.com>
2018-01-11 12:05:40 -0500

    adds some dummy data

---
+++ dummy.js
@@ -0,0 +1,21 @@
+const User = require('./models/user');
+const Post = require('./models/post');
+
+User.findOne({
+  where: {
+    id: 1
+  }
+}).then(author => {
+  // now! create a post!
+  Post.create({
+    title: 'First post!',
+    content: 'It was the best of times, it was the worst of times'
+  }).then(post => {
+    post.setUser(author);
+    // post.setComments([])
+    post.save();
+  })
+})
+
+
+
