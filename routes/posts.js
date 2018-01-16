bd0b88fd85fc79cab058b11ff5c65da5c394aa4c
Chris Aquino <chris.aquino@gmail.com>
2018-01-16 14:52:31 -0500

    adds create and retrieve of individual blog post

--- routes/posts.js
+++ routes/posts.js
@@ -20,11 +20,34 @@ router.route('/blog')
   .get((req, res) => {
     Post.findAll()
       .then(allPosts => {
-        res.send(allPosts);
+        // res.send(allPosts);
+        res.render('blog-list', {
+          posts: allPosts
+        });
       });
   })
   .delete((req, res) => {
     res.send('TODO: do this one');
   })
+
+router.route('/blog/new')
+  .get((req, res) => {
+    res.render('blog-form');
+  })
+
+router.route('/blog/:id')
+  .get((req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(result => {
+      res.render('blog-single', {
+        title: result.title,
+        content: result.content
+      });
+    })
+  })
+
 
 module.exports = router;
