59e14ba3c210e52788e7a1b8ba1b69f344b3d793
Chris Aquino <chris.aquino@gmail.com>
2018-01-16 13:59:37 -0500

    updates code slightly

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const posts = require('./routes/posts');
 app.use(posts);
 
-var expressHbs = require('express-handlebars');
+const expressHbs = require('express-handlebars');
 
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
